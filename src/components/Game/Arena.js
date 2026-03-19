0b9b9b3488b0d43427b02e5c48a1770515ff3214
Deepak Kumar <deepakindradeva@gmail.com>
2026-03-19 22:29:44 +0530

    refactor: remove cloud boundaries from arena and adjust fog distance in game scene.

--- src/components/Game/Arena.js
+++ src/components/Game/Arena.js
@@ -1,7 +1,6 @@
 // src/components/Game/Arena.js
 import React, { useMemo } from "react";
 import * as THREE from "three";
-import { Clouds, Cloud } from "@react-three/drei";
 
 const Arena = ({ width, height, color1 = "#66BB6A", color2 = "#43A047" }) => {
   const texture = useMemo(() => {
@@ -35,50 +34,6 @@ const Arena = ({ width, height, color1 = "#66BB6A", color2 = "#43A047" }) => {
         <planeGeometry args={[width, height]} />
         <meshStandardMaterial map={texture} roughness={0.8} />
       </mesh>
-
-      {/* Atmospheric Cloud Boundaries */}
-      <Clouds>
-        {/* Left Wall */}
-        <Cloud 
-          position={[-width / 2 - 2, 10, 0]} 
-          bounds={[5, 20, height + 10]} 
-          segments={120} 
-          volume={width}
-          color="#FFFFFF" 
-          opacity={0.9} 
-          speed={0.2} 
-        />
-        {/* Right Wall */}
-        <Cloud 
-          position={[width / 2 + 2, 10, 0]} 
-          bounds={[5, 20, height + 10]} 
-          segments={120} 
-          volume={width}
-          color="#FFFFFF" 
-          opacity={0.9} 
-          speed={0.2} 
-        />
-        {/* Top Wall */}
-        <Cloud 
-          position={[0, 10, -height / 2 - 2]} 
-          bounds={[width + 10, 20, 5]} 
-          segments={120} 
-          volume={height}
-          color="#FFFFFF" 
-          opacity={0.9} 
-          speed={0.2} 
-        />
-        {/* Bottom Wall */}
-        <Cloud 
-          position={[0, 10, height / 2 + 2]} 
-          bounds={[width + 10, 20, 5]} 
-          segments={120} 
-          volume={height}
-          color="#FFFFFF" 
-          opacity={0.9} 
-          speed={0.2} 
-        />
-      </Clouds>
     </group>
   );
 };
